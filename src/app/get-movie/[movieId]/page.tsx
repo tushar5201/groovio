601c60caf562c06aebbde260258b1ce575a73102
Tushar Lakadiya <tusharlakadiya@gmail.com>
2025-05-02 16:55:05 +0530

    frontend param fix

--- src/app/get-movie/[movieId]/page.tsx
+++ src/app/get-movie/[movieId]/page.tsx
@@ -1,10 +1,16 @@
 
-export default async function page({ params }: { params: { movieId: string } }) {
-  const movieId = params.movieId;
-  
+interface PageProps {
+  params: {
+    movieId: string;
+  };
+}
+
+export default async function Page({ params }: PageProps) {
+  const { movieId } = params;
+
   return (
     <div>
       <h1 className="text-white">{movieId}</h1>
     </div>
-  )
+  );
 }
